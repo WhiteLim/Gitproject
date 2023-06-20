02aaa4c7573d326c3b23316123fa0aa52287fb06
LeeDonSuk <132124666+WhiteLim@users.noreply.github.com>
2023-06-20 18:19:14 +0900

    err edit

--- clon/samsung-design/js/common.js
+++ clon/samsung-design/js/common.js
@@ -2,12 +2,8 @@ $(document).ready(function(){
     // header & footer 고정 호출
     $("#header").load("./head.html");
     $("#main").load("./sub-content/main.html");
-    var renewURL = window.location.pathname;
+    $("header nav li").removeClass('underline')
+    let renewURL = window.location.pathname;
     history.pushState(null, null, renewURL);
     $("#footer").load("./footer.html");
-
-
-    
-
-
 });
