39aa20138dd7fdfd7bc51a0e8678865921eebf37
LeeDonSuk <132124666+WhiteLim@users.noreply.github.com>
2023-06-20 10:13:43 +0900

    clon updata

---
+++ clon/samsung-design/js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    // header & footer 고정 호출
+    $("#header").load("./head.html");
+    $("#footer").load("./footer.html");
+
+
+    $("#search").click(function(){
+        alert("aa");
+    });
+
+
+});
