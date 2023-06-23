6dfc0dfdc5ec0a8104093c98ac02e1153256f265
LeeDonSuk <132124666+WhiteLim@users.noreply.github.com>
2023-06-23 17:37:15 +0900

    setting
    aa

--- clon/samsung-design/js/pop.js
+++ clon/samsung-design/js/pop.js
@@ -1,21 +1,32 @@
 let count = 0.4, last_scrollTop = 0;
 $(".pop_box div").scroll(function () {
     var tmp = $(this).scrollTop();
-    console.log (last_scrollTop,tmp)
     if (tmp > last_scrollTop)
     {
         
         if (count >= 1) {
             count = 1
         } else {
-            count += 0.01; 
+            count += 0.02; 
         }
         
         $("#reload").css("transform","scale(" + count + ")");
     
     } else {
-            count -= 0.01; 
+        if (count <= 0) {
+            count = 0.4
+        } else {
+            count -= 0.02; 
+        }
         $("#reload").css("transform","scale(" + count + ")");
     }
-    last_scrollTop = tmp;
+    
+    console.log(count)
+
+    if (count <= 0.5){
+        last_scrollTop = 0;
+    } else {
+        last_scrollTop = tmp;
+    }
+
 })
