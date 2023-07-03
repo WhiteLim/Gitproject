1d5adc2343cfd2cb808ec81e6bcfb5d09659d6cd
LeeDonSuk <132124666+WhiteLim@users.noreply.github.com>
2023-07-03 14:38:29 +0900

    dd

--- clon/samsung-design/js/common.js
+++ clon/samsung-design/js/common.js
@@ -6,6 +6,32 @@ $(document).ready(function(){
     let renewURL = window.location.pathname;
     history.pushState(null, null, renewURL);
     $("#footer").load("./footer.html");
+
+    if (window.history && history.pushState) {
+        addEventListener('load', function () {
+            addEventListener('popstate', function () {
+                let prs = document.location.href.split("?");
+                let result1 = prs[1].slice(-2, 8);
+
+                const li_lenght = document.querySelectorAll("header nav ul li");
+
+                li_lenght.forEach(function(v,k){
+                    let name = v.id;
+                    if(result1 == name){
+                        let id = name, idx=k+1;
+                        $("header nav li").removeClass('underline')
+                        $("header nav li:nth-of-type("+ idx +")").addClass('underline')
+                        $("#main").load("./sub-content/sub_page.html");
+                        let renewURL = window.location.pathname;
+                        renewURL += '?c-id='+id;
+                        history.pushState(null, null, renewURL);
+                    }
+                })
+
+            })
+        })
+    }
+
    
 });
 
