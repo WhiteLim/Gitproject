3524ce3eadf2f0b11191421e4cebaae6895b3884
LeeDonSuk <132124666+WhiteLim@users.noreply.github.com>
2023-07-03 18:19:18 +0900

    search updata

--- clon/samsung-design/js/search.js
+++ clon/samsung-design/js/search.js
@@ -41,44 +41,59 @@ function all_contents(){
 }
 
 function key_contents(k){
-    console.log(`${k}에 맞는 컨텐츠만 출력`)
     fetch("./js/data/data.json")
     .then((data) => data.json())
     .then(({ contents }) => {
         let html = '';
-        let cnt = 0;
+        let cnt = 0,count=0,q=0;
         contents.forEach(({ code, img_code, title, sub_title, date }) => {
+            let dd = contents.length
             let mix = title + sub_title + date
-
-            if (mix.toUpperCase().includes(k.toUpperCase()) == true){
-                if (cnt < 9) {
-                    html += /* html */ `
-                        <li class="sub" onclick="javascript:pops();">
-                                <figure>
-                                    <p><img src="./tools/${code}/${img_code}" alt="${img_code}"></p>
-                                    <figcaption>
-                                        <p class="m-title">${title}</p>
-                                        <p class="m-contents">${sub_title}</p>
-                                        <sapn class="date">${date}</sapn>
-                                </figure>
-                        </li>
-                    `;
-                } else {
-                    html += /* html */ `
-                        <li class="sub" onclick="javascript:pops();" style="display:none;">
-                                <figure>
-                                    <p><img src="./tools/${code}/${img_code}" alt="${img_code}"></p>
-                                    <figcaption>
-                                        <p class="m-title">${title}</p>
-                                        <p class="m-contents">${sub_title}</p>
-                                        <sapn class="date">${date}</sapn>
-                                </figure>
-                        </li>
-                    `;
-                    $(".mor").css("display","block");
-
+            if(mix.toUpperCase().includes(k.toUpperCase()) == true){
+                count --;
+                q ++;
+            } else {
+                count ++
+            }
+            if (count != dd){
+                if (mix.toUpperCase().includes(k.toUpperCase()) === true){
+                    const elH = document.querySelector(".search-page h1")
+                    elH.innerHTML=`Search Result for <span>${k}<b>${q}</b></span>`
+    
+                    if (cnt < 9) {
+                        html += /* html */ `
+                            <li class="sub" onclick="javascript:pops();">
+                                    <figure>
+                                        <p><img src="./tools/${code}/${img_code}" alt="${img_code}"></p>
+                                        <figcaption>
+                                            <p class="m-title">${title}</p>
+                                            <p class="m-contents">${sub_title}</p>
+                                            <sapn class="date">${date}</sapn>
+                                    </figure>
+                            </li>
+                        `;
+                    } else {
+                        html += /* html */ `
+                            <li class="sub" onclick="javascript:pops();" style="display:none;">
+                                    <figure>
+                                        <p><img src="./tools/${code}/${img_code}" alt="${img_code}"></p>
+                                        <figcaption>
+                                            <p class="m-title">${title}</p>
+                                            <p class="m-contents">${sub_title}</p>
+                                            <sapn class="date">${date}</sapn>
+                                    </figure>
+                            </li>
+                        `;
+                        $(".mor").css("display","block");
+    
+                    }
+                cnt ++;
                 }
-            cnt ++;
+            } else {
+                const elH = document.querySelector(".search-page h1")
+                elH.innerHTML=`Search Result for <span>${k}<b>${q}</b></span><br><br>
+                <span>죄송합니다. 검색 결과가 없습니다.</span>
+                <p>검색하신 단어가 정확한지 확인해 주시거나 다른 검색어를 사용해 주시기 바랍니다.</p>`
             }
         })
         $('.search-page ul').append(html);
